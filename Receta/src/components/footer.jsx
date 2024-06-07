9782c6c9770aec81a8606167ef34b9b45de903d5
Yeison Gonzalez <128742236+Uzumakii45@users.noreply.github.com>
2024-06-06 19:22:07 -0500

    Add files via upload
    Receta cargar con npm i

---
+++ Receta/src/components/footer.jsx
@@ -0,0 +1,10 @@
+
+function Footer() {
+  return(
+    <div className="mt-5 p-4 bg-dark text-white text-center">
+      <p>Footer - by Joan G. Peralta Collante</p>
+    </div>
+  )
+}
+
+export default Footer;
---
+++ Receta/src/components/header.jsx
@@ -0,0 +1,11 @@
+
+function Header(){
+    return(
+        <div className="p-5 bg-primary text-white text-center">
+            <h1>🥗🍝 Cocina 🧁 Colorida 🍲🍚</h1>
+            <p>¡El buen placer de la cocina!</p> 
+        </div>
+    )
+}
+
+export default Header;
